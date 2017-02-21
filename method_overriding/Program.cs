531e20e4abcb55c32a231cb0553fc822bb71919e
ilyas <ilyas@LAPTOP-NOQ3L7Q3>
2017-02-20 22:15:52 -0600

    Add project files.

---
+++ method_overriding/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_overriding
+{
+     class BaseClass
+        {
+            public virtual string YourCity()
+            {
+                return "Dallas";
+            }
+        }
+
+        class DerivedClass : BaseClass
+        {
+            public override string YourCity()
+            {
+                return "Irving";
+            }
+        }
+
+        class Program
+        {
+
+            static void Main(string[] args)
+            {
+                DerivedClass d = new DerivedClass();
+                string city = d.YourCity();
+                Console.WriteLine(city);
+                Console.ReadLine();
+            }
+     }
+}
+
+
